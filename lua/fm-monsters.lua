3dda102fc46131ac31b619ca931f4b54e16cf89a
bfaires <20690216+brianfaires@users.noreply.github.com>
2025-05-11 10:47:16 -0700

    fm refactor; packs

--- lua/fm-monsters.lua
+++ lua/fm-monsters.lua
@@ -1,15 +1,23 @@
-local function set_monster_fm(sign, monster_str)
-  local fm_str = "monster_warning:(?<!spectral )("..monster_str..
-      ")(?! (zombie|skeleton|simulacrum)).*comes? into view"
-  crawl.setopt("force_more_message " .. sign .. "= "..fm_str)
-end
+------------------- Dynamic force_mores config -------------------
+-- hp-specific force_mores() by gammafunk, edits by buehler
+-- WARNING: Never put a '}' on a line by itself. This breaks crawl's RC parser.
+-- Note: If you want an alert to silenceable by the fm_pack feature,
+-- It must be on an alert by itself (not part of a multi-monster pattern)
+local notify_fm = false -- Set to true to get a message when the fm change
+local turns_to_delay = 15 -- Turns before alerting for a pack monster again
 
--- This stops on all Uniques & Pan lords
+-- Stop on all Uniques & Pan lords
 crawl.setopt("force_more_message += monster_warning:" ..
               "(?-i:[A-Z]).*comes? into view")
 
--- Everything that always causes a more()
-local force_more_monsters = {
+-- Always screen flash
+local always_flash_screen_monsters = {
+  -- Noteworthy abilities
+  "vault warden", "vault guardian", "ghost crab"
+} -- always_flash_screen_monsters (do not remove this comment)
+
+-- Always more prompt
+local always_force_more_monsters = {
   -- High damage/speed
     "juggernaut", "orbs? of fire", "flayed ghost",
   -- Torment
@@ -21,64 +29,34 @@ local force_more_monsters = {
     "halazid warlock", "deep elf elementalist", "demonspawn corrupter",
     "elemental wellspring",
   --Dangerous abilities
-    "swamp worm", "vault warden", "air elemental", "wendingo",
+    "swamp worm", "air elemental", "wendingo", "wyrmhole",
     "torpor snail", "water nymph", "shambling mangrove", "iron giant",
     "starflower", "merfolk aquamancer", "deep elf knight", "wretched star",
   --Dangerous clouds
     "catoblepas", "death drake", "apocalypse crab", "putrid mouth",
-} -- force_more_monsters (do not remove this comment)
+} -- always_force_more_monsters (do not remove this comment)
 
+-- Only alert once per pack
+local fm_pack = {
+  "dream sheep", "shrike", "boggart", "floating eye"
+} -- fm_pack (do not remove this comment)
 
-------------------- force_mores w/ turn delay -------------------
--- The following monsters will only cause a force_more() once every # of turns; ie one alert per pack
-local fm_delayed = { "dream sheep", "shrike", "boggart", "floating eye" }
-local turns_to_delay = 15
+-- Once per pack, but alerts created through dynamic adds
+local fm_pack_no_init_add = {
+  "electric eel", "golden eye", "great orb of eyes"
+} -- fm_pack_no_init_add (do not remove this comment)
 
-local last_fm_turn = {}
-local monsters_to_mute = {}
-
-for v in iter.invent_iterator:new(fm_delayed) do
-  set_monster_fm("+", v)
-  last_fm_turn[v] = -1
-end
-
------- Hooks ------
-function c_message_fm_delayed(text, _)
-  for v in iter.invent_iterator:new(fm_delayed) do
-    if text:find(v..".*comes? into view") and last_fm_turn[v] == -1 then
-      last_fm_turn[v] = you.turns()
-      monsters_to_mute[#monsters_to_mute+1] = v
-    end
-  end
-end
-
-function ready_fm_delayed()
-  for v in iter.invent_iterator:new(monsters_to_mute) do
-    set_monster_fm("-", v)
-  end
-  monsters_to_mute = {}
-
-  for v in iter.invent_iterator:new(fm_delayed) do
-    if you.turns() == last_fm_turn[v] + turns_to_delay then
-      set_monster_fm("+", v)
-      last_fm_turn[v] = -1
-    end
-  end
-end
-
-
-------------------- Dynamic force_mores config -------------------
--- hp-specific force_mores() by gammafunk, edits by buehler
-local notify_fm = false -- Set to true to get a message when the fm change
 
 local fm_patterns = {
-  -- Fast, early game Dungeon problems for chars with low mhp.
-  {name = "30hp", cond = "hp", cutoff = 30, pattern = "hound"},
+  -- Early game Dungeon problems for chars with low mhp. (adder defined below)
+  {name = "30hp", cond = "hp", cutoff = 30,
+      pattern = "hound|gnoll"},
 
   -- Monsters dangerous until a certain point
-  {name = "xl_7", cond = "xl", cutoff = 7, pattern = "orc wizard"},
-  {name = "xl_12", cond = "xl", cutoff = 12, pattern = "hydra|bloated husk"},
-
+  {name = "xl_7", cond = "xl", cutoff = 7,
+      pattern = "orc wizard"},
+  {name = "xl_12", cond = "xl", cutoff = 12,
+      pattern = "hydra|bloated husk"},
 
   -- Monsters that can hit for ~50% of hp from range with unbranded attacks
   {name = "40hp", cond = "hp", cutoff = 40,
@@ -86,43 +64,48 @@ local fm_patterns = {
   {name = "50hp", cond = "hp", cutoff = 50,
       pattern = "orc high priest|manticore" },
   {name = "70hp", cond = "hp", cutoff = 70,
-      pattern = "meliai|yaktaur(?! captain)|cyclops|centaur(?! warrior)" },
+      pattern = "yaktaur(?! captain)|cyclops|centaur(?! warrior)" },
+  {name = "70hp_melai", cond = "hp", cutoff = 70,
+      pattern = "meliai"},
   {name = "80hp", cond = "hp", cutoff = 80,
       pattern = "gargoyle" },
   {name = "90hp", cond = "hp", cutoff = 90,
       pattern = "deep elf archer|tengu conjurer" },
   {name = "110hp", cond = "hp", cutoff = 110,
-      pattern = {"centaur warrior|yaktaur captain|hellion|eye of devastation|sun moth"..
-                  "deep elf high priest|deep troll earth mage|stone giant|cacodemon"} },
+      pattern = "centaur warrior|yaktaur captain|hellion|eye of devastation|sun moth"..
+                  "deep elf high priest|deep troll earth mage|stone giant|cacodemon" },
   {name = "120hp", cond = "hp", cutoff = 120,
       pattern = "quicksilver (dragon|elemental)|magenta draconian|thorn hunter" },
   {name = "160hp", cond = "hp", cutoff = 160,
-      pattern = {"brimstone fiend|deep elf sorcerer"..
-              "hell sentinal|war gargoyle|draconian (knight|scorcher)"} },
+      pattern = "brimstone fiend|deep elf sorcerer"..
+              "hell sentinal|war gargoyle|draconian (knight|scorcher)" },
   {name = "200hp", cond = "hp", cutoff = 200,
       pattern = "(draconian|deep elf) annihilator|iron (dragon|elemental)" },
 
   -- Monsters that can crowd-control you without sufficient willpower
   -- Cutoff ~10% for most spells; lower for more significant spells like banish
   {name = "willpower2", cond = "will", cutoff = 2,
-      pattern = "basilisk|naga ritualist|vampire(?! bat)(?! mage)(?! mosquito)" },
+      pattern = "basilisk|naga ritualist|vampire(?! bat)(?! mage)(?! mosquito)|sphinx marauder" },
   {name = "willpower3", cond = "will", cutoff = 3,
-      pattern = {"deep elf (demonologist|sorcerer|archer)|(?<!orc )wizard|"..
-              "merfolk siren|fenstrider witch|great orb of eyes|cacodemon|"..
-              "imperial myrmidon|sphinx|nagaraja|draconian shifter|"..
-              "orange crystal statue|glowing orange brain|orc sorcerer|"..
-              "ogre mage|satyr|vault sentinel|iron elemental|golden eye|"..
-              "death knight|vampire knight" } },
+      pattern = "deep elf (demonologist|sorcerer|archer)|(?<!orc )wizard|"..
+              "merfolk siren|fenstrider witch|cacodemon|"..
+              "imperial myrmidon|guardian sphinx|nagaraja|draconian shifter|"..
+              "glowing orange brain|orc sorcerer|"..
+              "ogre mage|satyr|vault sentinel|iron elemental|"..
+              "death knight|vampire knight" },
+  {name = "willpower3_great_orb_of_eyes", cond = "will", cutoff = 3,
+      pattern = "great orb of eyes" },
+  {name = "willpower3_golden_eye", cond = "will", cutoff = 3,
+      pattern = "golden eye" },
   {name = "willpower4", cond = "will", cutoff = 4,
       pattern = "merfolk avatar|tainted leviathan|nargun" },
 
   -- Malmutate without rMut
   {name = "malmutate", cond = "mut", cutoff = 1,
       pattern = "cacodemon|neqoxec|shining eye" },
-
   -- Brain feed with low int
   {name = "brainfeed", cond = "int", cutoff = 6,
-      pattern = "glowing orange brain|neqoxec|orange crystal statue" },
+      pattern = "glowing orange brain|neqoxec" },
 
   -- Alert if no resist and HP below cutoff
   {name = "pois_30", cond = "pois", cutoff = 30,
@@ -188,24 +171,40 @@ local fm_patterns = {
 } -- end fm_patterns (do not remove this comment)
 ------------------- End config section -------------------
 
-local fm_mon_str = nil
-for v in iter.invent_iterator:new(force_more_monsters) do
-  if not fm_mon_str then
-    fm_mon_str = v
-  else
-    fm_mon_str = fm_mon_str .. "|" .. v
+local active_fm = {} -- which ones are on
+local active_fm_index = {} -- lookup table for speed
+local monsters_to_mute = {} -- which packs are muted
+local last_fm_turn = {} -- when the mute started
+
+
+local function set_monster_option(sign, monster_str, option)
+  local fm_str = "monster_warning:(?<!spectral )("..monster_str..
+      ")(?! (zombie|skeleton|simulacrum)).*comes? into view"
+  crawl.setopt(option .. " " .. sign .. "= "..fm_str)
+end
+
+local function set_monster_fm(sign, monster_str)
+  set_monster_option(sign, monster_str, "force_more_message")
+end
+
+local function set_monster_flash(sign, monster_str)
+  set_monster_option(sign, monster_str, "flash_screen_message")
+end
+
+local function set_all(monster_list, option)
+  local mon_str = nil
+  for _, v in ipairs(monster_list) do
+    if not mon_str then
+      mon_str = v
+    else
+      mon_str = mon_str .. "|" .. v
+    end
   end
-end
-set_monster_fm("+", fm_mon_str)
-
--- Keep track of active force_mores()
-local active_fm = {}
-for _ in iter.invent_iterator:new(fm_patterns) do
-  active_fm[#active_fm + 1] = false
+  set_monster_option("+", mon_str, option)
 end
 
--- Util for checks against resistance and hp
 local function get_three_pip_action(active, hp, cutoff, res)
+  -- Util for checks against resistance and hp
   local div = res+1
   if div == 4 then div = 5 end
 
@@ -217,7 +216,7 @@ local function get_three_pip_action(active, hp, cutoff, res)
 end
 
 
-------------------- Hook -------------------
+------------------- Hooks -------------------
 function ready_force_mores()
   local activated = {}
   local deactivated = {}
@@ -288,7 +287,7 @@ function ready_force_mores()
       if v.name then fm_name = v.name end
 
       if type(v.pattern) == "table" then
-        for p in iter.invent_iterator:new(v.pattern) do
+        for _, p in ipairs(v.pattern) do
           if not fm_mon_str then
             fm_mon_str = p
           else
@@ -302,18 +301,85 @@ function ready_force_mores()
       set_monster_fm(action, fm_mon_str)
       active_fm[i] = not active_fm[i]
 
-      if action == "+" then
-        activated[#activated + 1] = fm_name
-      elseif action == "-" then
-        deactivated[#deactivated + 1] = fm_name
+      if notify_fm then
+        if action == "+" then
+          activated[#activated + 1] = fm_name
+        elseif action == "-" then
+          deactivated[#deactivated + 1] = fm_name
+        end
       end
     end
   end
 
-  if #activated > 0 and notify_fm then
-    crawl.mpr("Activating force_mores: " .. table.concat(activated, ", "), "plain")
+  if notify_fm then
+    if #activated > 0 then
+      crawl.mpr("Activating force_mores: " .. table.concat(activated, ", "), "plain")
+    end
+    if #deactivated > 0 then
+      crawl.mpr("Deactivating force_mores: " .. table.concat(deactivated, ", "), "plain")
+    end
   end
-  if #deactivated > 0 and notify_fm then
-    crawl.mpr("Deactivating force_mores: " .. table.concat(deactivated, ", "), "plain")
+end
+
+function c_message_fm_pack(text, _)
+  -- Identifies when a mute should be turned on
+  if not text:find("comes? into view") then return end
+  for _, v in ipairs(fm_pack) do
+    if text:find(v) and last_fm_turn[v] == -1 then
+      last_fm_turn[v] = you.turns()
+      monsters_to_mute[#monsters_to_mute+1] = v
+    end
   end
+end
+
+function ready_fm_pack()
+  -- Put pending mutes into effect
+  for _, v in ipairs(monsters_to_mute) do
+    set_monster_fm("-", v)
+  end
+  monsters_to_mute = {}
+
+  -- Remove mutes that have expired
+  for _, v in ipairs(fm_pack) do
+    if you.turns() == last_fm_turn[v] + turns_to_delay then
+      set_monster_fm("+", v)
+      last_fm_turn[v] = -1
+    end
+  end
+
+  -- For no-init pack monsters, just deactivate the fm
+  for _, v in ipairs(fm_pack_no_init_add) do
+    if you.turns() == last_fm_turn[v] + turns_to_delay then
+      active_fm[active_fm_index[v]] = false
+      last_fm_turn[v] = -1
+    end
+  end
+end
+
+
+------ Startup code ------
+set_all(always_flash_screen_monsters, "flash_screen_message")
+set_all(always_force_more_monsters, "force_more_message")
+
+-- Init packs
+for _, v in ipairs(fm_pack) do
+  set_monster_fm("+", v)
+  last_fm_turn[v] = -1
+end
+
+for _,v in ipairs(fm_pack_no_init_add) do
+  fm_pack[#fm_pack+1] = v
+  last_fm_turn[v] = -1
+end
+
+-- Build table of indexes, used for fm_pack logic
+for i,v in ipairs(fm_patterns) do
+  if not v.pattern:find("|") then
+    active_fm_index[v.pattern] = i
+  end
+end
+
+-- Init all active_fm to false
+for _,_ in ipairs(fm_patterns) do
+  active_fm[#active_fm + 1] = false
 end
