b44ae1d91fa059af0411429e6b83c2e5d4f5c224
bfaires <20690216+brianfaires@users.noreply.github.com>
2025-07-13 16:24:39 -0700

    bug fix safe-consumables

--- lua/features/safe-consumables.lua
+++ lua/features/safe-consumables.lua
@@ -5,24 +5,29 @@
 
 local NO_INSCRIPTION_NEEDED = {
   "acquirement", "amnesia", "blinking", "brand weapon", "enchant armour", "enchant weapon",
-  "identify", "immolation", "noise", "vulnerability", "attraction", "lignification", "mutation"}
+  "identify", "immolation", "noise", "vulnerability", "attraction", "lignification", "mutation"
+} -- NO_INSCRIPTION_NEEDED (do not remove this comment)
 
 ------------------- Hook -------------------
 function ready_safe_consumables()
   -- Remove the default "!r" and "!q" inscriptions after identify
   for inv in iter.invent_iterator:new(items.inventory()) do
     local inv_class = inv.class(true)
+    local st = inv.subtype()
     if inv_class == "scroll" then
-      local st = inv.subtype()
       if (st == "poison" and you.res_poison() > 0)
         or (st == "torment" and you.torment_immune())
         or util.contains(NO_INSCRIPTION_NEEDED, st) then
-          if inv.inscription:find("%!r") then inv.inscribe(inv.inscription:gsub("%!r", ""), false) end
+          if inv.inscription:find("%!r") then
+            inv.inscribe(inv.inscription:gsub("%!r", ""), false)
+          end
       elseif not inv.inscription:find("%!r") then inv.inscribe("!r")
       end
     elseif inv_class == "potion" then
       if util.contains(NO_INSCRIPTION_NEEDED, st) then
-        if inv.inscription:find("%!q") then inv.inscribe(inv.inscription:gsub("%!q", ""), false) end
+        if inv.inscription:find("%!q") then 
+          inv.inscribe(inv.inscription:gsub("%!q", ""), false)
+        end
       elseif not inv.inscription:find("%!q") then inv.inscribe("!q")
       end
     end
