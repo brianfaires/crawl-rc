f5f64f6d8a8aa9ec84e6e502bf8eab807b82655b
bfaires <20690216+brianfaires@users.noreply.github.com>
2025-07-13 15:47:20 -0700

    var rename for consistency

--- lua/features/fully-recover.lua
+++ lua/features/fully-recover.lua
@@ -6,14 +6,14 @@ local STATUSES_TO_WAIT_OFF = { "berserk", "short of breath", "corroded", "vulner
 local recovery_start_turn
 local explore_after_recovery
 
-local function abort_full_recovery()
+local function abort_fully_recover()
   recovery_start_turn = 0
   crawl.setopt("message_colour -= mute:You start waiting.")
   explore_after_recovery = false
   you.stop_activity()
 end
 
-local function finish_full_recovery()
+local function finish_fully_recover()
   local turns = you.turns() - recovery_start_turn
   local msg = with_color(COLORS.lightgreen, "Fully recovered") .. string.format(" (%d turns)", turns)
   crawl.mpr(msg)
@@ -47,7 +47,7 @@ local function fully_recovered()
   return true
 end
 
-local function start_full_recovery()
+local function start_fully_recover()
   recovery_start_turn = CACHE.turn
   crawl.setopt("message_colour += mute:You start waiting.")
 end
@@ -61,11 +61,11 @@ function init_fully_recover()
   
   crawl.setopt("runrest_ignore_message += recovery:.*")
   crawl.setopt("runrest_ignore_message += duration:.*")
-  crawl.setopt("macros += M o ===macro_explore_full_recovery")
+  crawl.setopt("macros += M o ===macro_explore_fully_recover")
 end
 
 -- Attach full recovery to auto-explore
-function macro_explore_full_recovery()
+function macro_explore_fully_recover()
   if fully_recovered() then
     crawl.do_commands({"CMD_EXPLORE"})
   else
@@ -79,23 +79,23 @@ end
 function c_message_fully_recover(text, channel)
   if channel == "plain" then
     if text:find("You start waiting") or text:find("You start resting") then
-      if not fully_recovered() then start_full_recovery() end
+      if not fully_recovered() then start_fully_recover() end
     end
   elseif recovery_start_turn > 0 then
-    if channel == "timed_portal" then abort_full_recovery()
-    elseif fully_recovered() then finish_full_recovery()
+    if channel == "timed_portal" then abort_fully_recover()
+    elseif fully_recovered() then finish_fully_recover()
     end
   end
 end
 
-function ch_stop_running_full_recovery(kind)
-  if kind == "run" and not fully_recovered() then start_full_recovery() end
+function ch_stop_running_fully_recover(kind)
+  if kind == "run" and not fully_recovered() then start_fully_recover() end
 end
 
 function ready_fully_recover()
   if recovery_start_turn > 0 then
-    if fully_recovered() then finish_full_recovery()
-    elseif not you.feel_safe() then abort_full_recovery()
+    if fully_recovered() then finish_fully_recover()
+    elseif not you.feel_safe() then abort_fully_recover()
     else crawl.do_commands({"CMD_SAFE_WAIT"})
     end
   else
