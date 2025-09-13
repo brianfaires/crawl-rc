7e3fdaada5749d273a16e7fe55f5ce7c2e8c93af
bfaires <20690216+brianfaires@users.noreply.github.com>
2025-09-13 16:42:32 -0700

    build updates

--- build/concat_rc.py
+++ build/concat_rc.py
@@ -1,9 +1,9 @@
-import re
 import os
 
 
-INIT_FILE_NAME = "init.txt"
-OUT_SUBDIR = "bin/"
+INIT_FILE_NAME = "../rc/init.txt"
+OUT_SUBDIR = "../bin/"
+CORE_SUBDIR = "../lua/core/"
 OUT_FILE_NAME = OUT_SUBDIR +"buehler.rc"
 CORE_FILE_NAME = OUT_SUBDIR +"core.rc"
 REPO_ROOT = "crawl-rc/"
@@ -12,12 +12,14 @@ LUA_PREFX = f"lua_file = {REPO_ROOT}"
 LOAD_LUA_PREFIX = "loadfile("
 
 def write_header(cur_file, file_name, outfile):
+  file_name = file_name.replace("../", "")
   is_lua = cur_file and cur_file.name.endswith(".lua")
   comment_char = "-" if is_lua else "#"
   outfile.write(f"\n{comment_char * 35} Begin {file_name} {comment_char * 35}\n")
   outfile.write(f"{comment_char * 15} https://github.com/brianfaires/crawl-rc/ {comment_char * 15}\n")
 
 def write_footer(cur_file, file_name, outfile):
+  file_name = file_name.replace("../", "")
   is_lua = cur_file and cur_file.name.endswith(".lua")
   comment_char = "-" if is_lua else "#"
   outfile.write(f"\n{comment_char * 31} End {file_name} {comment_char * 31}\n")
@@ -26,15 +28,15 @@ def write_footer(cur_file, file_name, outfile):
 def write_resume(cur_file, outfile):
   is_lua = cur_file.name.endswith(".lua")
   comment_char = "-" if is_lua else "#"
-  outfile.write(f"\n{comment_char * 2} (Resuming {cur_file.name}) {comment_char*2}\n")
+  outfile.write(f"\n{comment_char * 2} (Resuming {cur_file.name.replace('../', '')}) {comment_char*2}\n")
 
 def is_new_file(line, prev_files):
   # this is an artefact from when I used loadfile() for dependency tracking
   if line.startswith(LOAD_LUA_PREFIX):
     line = line.split('"')[1]
-  file_name = line.replace(RC_PREFIX, "").strip()
-  file_name = file_name.replace(LUA_PREFX, "").strip()
-  file_name = file_name.replace(REPO_ROOT, "").strip()
+  file_name = line.replace(RC_PREFIX, "../").strip()
+  file_name = file_name.replace(LUA_PREFX, "../").strip()
+  file_name = file_name.replace(REPO_ROOT, "../").strip()
 
   if file_name not in prev_files:
     prev_files.append(file_name)
@@ -94,7 +96,7 @@ print(f"\nWriting to {OUT_FILE_NAME}\n")
 os.makedirs(os.path.dirname(CORE_FILE_NAME), exist_ok=True)
 with open(CORE_FILE_NAME, 'w') as outfile:
   for filename in [ "config.lua", "constants.lua", "util.lua", "data.lua", "brc.lua" ]:
-    with open(f"lua/core/{filename}", 'r') as readfile:
+    with open(f"{CORE_SUBDIR}{filename}", 'r') as readfile:
       write_header(None, filename, outfile)
       outfile.write("{\n")
       recurse_write_lines(readfile, outfile, [])
