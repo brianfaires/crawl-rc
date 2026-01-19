aad48887e1e6a2f2962bb9d88da27b277881c793
bfaires <20690216+brianfaires@users.noreply.github.com>
2026-01-19 00:01:31 -0800

    block lignification over water/lave

--- lua/features/answer-prompts.lua
+++ lua/features/answer-prompts.lua
@@ -7,11 +7,23 @@
 f_answer_prompts = {}
 f_answer_prompts.BRC_FEATURE_NAME = "answer-prompts"
 
+---- Local constants ----
+local BAD_FOR_TREES = { "deep_water", "lava", "open_sea", "endless_lava" }
+
 ---- Crawl hook functions ----
 function f_answer_prompts.c_answer_prompt(prompt)
   if prompt == "Die?" then return false end
+
   if prompt:contains("cheaper one?") and you.branch() ~= "Bazaar" then
     BRC.mpr.yellow("Replacing shopping list items")
     return true
+  end
+
+  if prompt:contains("quaff the potion of lignification") then
+    local feat = view.feature_at(0,0)
+    if feat and util.contains(BAD_FOR_TREES, feat) then
+      BRC.mpr.warning("Blocking lignification over " .. feat)
+      return false
+    end
   end
 end
